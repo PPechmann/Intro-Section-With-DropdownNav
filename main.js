9f64ccac9bbffecef6198d479ed16f9847f4b379
Patrick <pechmann.patrick93@gmail.com>
2022-09-01 21:37:07 +0200

    added media queries and js functionality for collapsable menu

--- main.js
+++ main.js
@@ -8,16 +8,34 @@ const registerBtn = document.querySelector(".nav__registerBtn");
 const dropdownFeatures = document.querySelector(".dropdownFeatures");
 const dropdownCompany = document.querySelector(".dropdownCompany");
 
-const navArrowFeatures = document.querySelectorAll(".nav__arrow")[0];
-const navArrowCompany = document.querySelectorAll(".nav__arrow")[1];
 
 const mainContent = document.querySelector(".container");
 
 const navbar = document.querySelector(".nav");
+const navBurguer = document.querySelector(".nav__burguer");
+
+const sideNav = document.querySelector(".sideNav--hidden");
+
+const overlay = document.querySelector(".overlay");
+
+const sideNavFeat = document.querySelector(".sideNavFeatLink");
+const sideNavCompany = document.querySelector(".sideNavCompanyLink");
+
+const sideNavArrowFeatures = document.querySelectorAll(".sideNav__arrow")[0];
+const sideNavArrowCompany = document.querySelectorAll(".sideNav__arrow")[1];
+const navArrowFeatures = document.querySelectorAll(".nav__arrow")[0];
+const navArrowCompany = document.querySelectorAll(".nav__arrow")[1];
+
+const sideNavFeatList = document.querySelector(".sideNavFeat__list");
+const sideNavCompanyList = document.querySelector(".sideNavCompany__list");
 
 
 
 
+
+
+
+/*detects if another menu is active, then closes it*/
 function detectDropdown(e) {
     if(e.target !== dropdownCompany || dropdownFeatures) {
         if(dropdownCompany.classList.contains("toggleDropdownCompany")){
@@ -32,9 +50,8 @@ function detectDropdown(e) {
 }
 
 
-
+/*listener for features dropdown click*/
 featuresLink.addEventListener("click", (e) => {
-
     navArrowFeatures.classList.toggle("invertArrow");
     dropdownFeatures.classList.toggle("toggleDropdownFeat");
     if(dropdownCompany.classList.contains("toggleDropdownCompany")){
@@ -44,7 +61,7 @@ featuresLink.addEventListener("click", (e) => {
     });
 
 
-
+/*listener for company dropdown click*/
 companyLink.addEventListener("click", (e) => {
     navArrowCompany.classList.toggle("invertArrow");
     dropdownCompany.classList.toggle("toggleDropdownCompany");
@@ -54,6 +71,7 @@ companyLink.addEventListener("click", (e) => {
     }
 
 });
+
 
 mainContent.addEventListener("click", (e) => {
     detectDropdown(e);
@@ -73,7 +91,39 @@ loginLink.addEventListener("click", (e) => {
 
 registerBtn.addEventListener("click", (e) => {
     detectDropdown(e);
-})
+});
+
+
+/*listener for the tab and phone size burger menu*/
+navBurguer.addEventListener("click", (e) => {
+    sideNav.style.animation = "fadeIn .3s 1";
+    sideNav.classList.toggle("sideNav--visible");
+    navBurguer.classList.toggle("nav__burguer--closed");
+    overlay.classList.toggle("activateOverlay");
+});
+
+
+
+
+
+/*listeners for the collapsable menu links (features and company)*/
+sideNavFeat.addEventListener("click", () => {
+    sideNavArrowFeatures.classList.toggle("invertArrow");
+    sideNavFeatList.classList.toggle("sideNavFeat__list--visible");
+    if(sideNavCompanyList.classList.contains("sideNavCompany__list--visible")){
+        sideNavArrowCompany.classList.toggle("invertArrow");
+        sideNavCompanyList.classList.toggle("sideNavCompany__list--visible");
+    }
+});
+
+sideNavCompany.addEventListener("click", () => {
+    sideNavArrowCompany.classList.toggle("invertArrow");
+    sideNavCompanyList.classList.toggle("sideNavCompany__list--visible");
+    if(sideNavFeatList.classList.contains("sideNavFeat__list--visible")){
+        sideNavArrowCompany.classList.toggle("invertArrow");
+        sideNavFeatList.classList.toggle("sideNavFeat__list--visible");
+    }
+});
 
 
 
